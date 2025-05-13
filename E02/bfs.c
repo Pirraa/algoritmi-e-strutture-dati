9c4ab3833d8eec88771bf30ef9f56b3889401878
Pirraa <pirrafranci2004@gmail.com>
2025-05-13 11:15:17 +0200

    esercizio2 algoritmi

--- E02/bfs.c
+++ E02/bfs.c
@@ -34,20 +34,31 @@ int bfsBipartito(Lista *grafo, int *colore, int start, int n)
     int *queue = malloc(n * sizeof(int));
     int front = 0, rear = 0;
 
+    //front: indice elemento da processare (esce dalla coda)
+    //rear: indice elemento da inserire (entra nella coda)
+    
+    //setta colore sorgente a 0 e la inserisce in coda(enqueue)
     colore[start] = 0;
     queue[rear++] = start;
 
+    //finchè ci sono elementi nella coda 
     while (front < rear)
     {
+        //estraggo nodo dalla coda (dequeue)
         int u = queue[front++];
+        //il for scorre la lista di adiacenza del nodo u
         for (Nodo *p = grafo[u]; p != NULL; p = p->next)
         {
+            //prendo indice del pugile dal nodo e controllo il colore
             int v = p->pugile;
+            //se il pugile non è stato colorato, lo coloro con il colore opposto a quello di u e lo inserisco in coda
             if (colore[v] == -1)
             {
-                colore[v] = 1 - colore[u];
+                colore[v] = 1 - colore[u]; //1-0=1 1-1=0
                 queue[rear++] = v;
             }
+            //se il pugile è già colorato e ha lo stesso colore di u, il grafo non è bipartito
+            //se il pugile è già colorato e ha un colore diverso da u, non faccio nulla
             else if (colore[v] == colore[u])
             {
                 free(queue);
@@ -118,6 +129,10 @@ int main()
     }
 
     // Array colori per bipartizione (-1 = non assegnato)
+    //non metto il colore nel nodo perchè il colore si riferisce ai nodi del grafo e non agli archi
+    //la lista di adiacenza è un array di liste, quindi non posso mettere il colore nel nodo
+    //nella lista di adiacenza posso avere più nodi che fanno riferimento allo stesso pugile (ogni arco rappresentato due volte)
+    //se salvo il colore nel nodo, non posso avere più nodi con lo stesso pugile ma con colori diversi, quindi incoerenza nei dati
     int *colore = malloc(num_pugili * sizeof(int));
     for (int i = 0; i < num_pugili; i++)
         colore[i] = -1;
@@ -125,6 +140,8 @@ int main()
     int bipartito = 1;
 
     // gestisce grafi non connessi
+    //cavallo di troia come depthfirstsearch che richiama la visita per ogni componente connessa nel caso il grafo sia sconnesso
+    // per ogni nodo non visitato, chiama bfsBipartito
     for (int i = 0; i < num_pugili && bipartito; i++)
     {
         if (colore[i] == -1)
@@ -136,7 +153,8 @@ int main()
 
     if (bipartito)
     {
-        printf("È possibile dividere i pugili in due gruppi.\n");
+        fprintf(output, "TRUE");
+        /*printf("È possibile dividere i pugili in due gruppi.\n");
         printf("Gruppo 0:\n");
         for (int i = 0; i < num_pugili; i++)
             if (colore[i] == 0)
@@ -145,12 +163,13 @@ int main()
         for (int i = 0; i < num_pugili; i++)
             if (colore[i] == 1)
                 printf("%d ", i);
-        printf("\n");
+        printf("\n");*/
     }
     else
     {
-        printf("Non è possibile dividere i pugili in due gruppi.\n");
-        printf("Non bipartibile\n");
+        fprintf(output, "FALSE");
+        /*printf("Non è possibile dividere i pugili in due gruppi.\n");
+        printf("Non bipartibile\n");*/
     }
 
     // stampa debug (facoltativa)
